13dc48c4a135edae7937c539815a7808d340e6a4
chan-nni <rkdcksal02@gmail.com>
2024-02-02 17:17:36 +0900

    [Workshop4] Lotto 자동 번호 생성기

---
+++ java_workshop/src/LottoAryMain.java
@@ -0,0 +1,21 @@
+import workshop3.lotto.exercise.ExerciseLotto;
+
+public class LottoAryMain {
+
+	public static void main(String[] args) {
+		ExerciseLotto lotto = new ExerciseLotto();
+		
+		// Mission 01.
+		int [] ary = lotto.generateLottoNumbers() ;
+		/*
+		for(int idx = 0 ; idx < ary.length ; idx++) {
+			System.out.print(ary[idx]+" "); 
+		}
+		*/
+		lotto.displayLottoNumbers(ary) ; 
+		
+		
+		
+	}
+
+}
